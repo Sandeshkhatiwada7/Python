12b505f9754a1fc27309dfd6638d17a306f09844
Sandesh Khatiwada <sandeshkhatiwada71@gmail.com>
2026-08-06 12:34:17 +0545

    finding greatest number

---
+++ greatestnum.py
@@ -0,0 +1,13 @@
+a=int(input("enter a number:"))
+b=int(input("enter a number:"))
+c=int(input("enter a number:"))
+d=int(input("enter a number:"))
+if (a>b and a>c and a>d):
+    print("a is the greatest number")
+elif (b>a and b>c and b>d):
+  print("b is greatest number")
+elif (c>a and c>b and c>d):
+   print("c is greatest")
+else:
+      print("d is greatest")
+   
