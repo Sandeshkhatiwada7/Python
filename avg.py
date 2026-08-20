6a7923ef27de54654989e6db2bff6d11bc2ef6b3
Sandesh Khatiwada <sandeshkhatiwada71@gmail.com>
2026-08-20 15:43:28 +0545

    to calculate avg of user inputted numbers

---
+++ avg.py
@@ -0,0 +1,9 @@
+def avg(a,b,c):
+    avg=(a+b+c)/3
+    return avg
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+result=avg(num1,num2,num3)
+print("the average is:",result)
+
